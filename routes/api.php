bf106c9fe728d5ba6b8037913a489335fb210a8e
DavidDem249 <daoudadembele215@gmail.com>
2021-02-22 15:47:29 +0100

    Cors Origin

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
 header('Access-Control-Allow-Origin: *');
 
-
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StoreController;
